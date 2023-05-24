0f1a946d6890b91faad44b710cc1f5da88307168
EmersonRabelo <emersonrabelo1998@gmail.com>
2023-05-24 16:33:30 -0300

    Add: Learning readonly concept

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,16 +1,15 @@
 export class Negociacao {
-    private _data: Date;
+
+    // Aprendendo o conceito readonly
+    public readonly data: Date;
+    
     private _quantidade: number;
     private _valor: number;
 
     constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
+        this.data = data;
         this._quantidade = quantidade;
         this._valor = valor;
-    }
-
-    get data(): Date {
-        return this._data;
     }
 
     get quantidade(): number {
